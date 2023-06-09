ba7f02f97841652cff738d760781dcc52b11e766
SolarPowerWu <solarpowerwu@gmail.com>
2023-06-09 12:01:05 -0500

    debug

--- model.py
+++ model.py
@@ -2,15 +2,17 @@ import torch
 import torch.nn as nn
 
 class ChatBot(nn.Module):
-    def __init__(self, pretrained_model, decoder_network, tokenizer, bos_token_id, eos_token_id):
+    def __init__(self, pretrained_model, decoder_network, tokenizer, bos_token_id, eos_token_id, device):
         super(ChatBot, self).__init__()
         self.llm = pretrained_model
         self.decoder = decoder_network
         self.tokenizer = tokenizer
         self.bos = bos_token_id
         self.eos = eos_token_id
+        self.device = device
 
     def forward(self, input_ids, attention_mask):
+        input_ids = input_ids.to(self.device)
         with torch.no_grad():
             outputs = self.llm(input_ids=input_ids, attention_mask=attention_mask)
             encoded_input = outputs.last_hidden_state
