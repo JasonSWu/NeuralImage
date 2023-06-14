7cd7ba8c007c6964c88209aa5f4671e5cc8c6b52
SolarPowerWu <solarpowerwu@gmail.com>
2023-06-14 00:25:49 -0500

    swap to chinese model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from tqdm import tqdm
 from datasets import load_dataset
 from transformers import AutoConfig, AutoTokenizer, XLMRobertaModel
+from transformers import BertModel, BertTokenizer
 from model import ChatBot, MyDecoder
 from data import process_data
 import os
@@ -48,9 +49,9 @@ def train(base_llm, decoder, train_dataloader, num_epochs, PAD_IDX, device="cuda
 
 device = torch.device("cuda")
 
-config = AutoConfig.from_pretrained("xlm-roberta-base")
-tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
-pretrained_model = XLMRobertaModel.from_pretrained("xlm-roberta-base", is_decoder=False)
+config = AutoConfig.from_pretrained("bert-base-chinese")
+tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
+pretrained_model = BertModel.from_pretrained("bert-base-chinese")
 pretrained_model.eval()
 pretrained_model.requires_grad_(False)
 hidden_size = config.hidden_size
@@ -58,7 +59,7 @@ vocab_size = config.vocab_size
 
 data = load_dataset('silver/personal_dialog')
 train_data = process_data(data['train'], tokenizer, 10000)
-decoder_layer = nn.TransformerDecoderLayer(d_model=hidden_size, nhead=8, batch_first=True)
+decoder_layer = nn.TransformerDecoderLayer(d_model=hidden_size, nhead=6, batch_first=True)
 norm_layer = nn.LayerNorm(hidden_size)
 decoder = MyDecoder(nn.TransformerDecoder(decoder_layer, num_layers = 4, norm = norm_layer), hidden_size, vocab_size)
 decoder = train(pretrained_model, decoder, train_data, 20, config.pad_token_id, device)
