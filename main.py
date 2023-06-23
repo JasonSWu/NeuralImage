54d6f02dce670e1a4effffc8b7b8710492372fa0
SolarPowerWu <solarpowerwu@gmail.com>
2023-06-22 21:44:02 -0500

    partially debugged memory prototype

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ import os
 def upper_tri_mask(n):
   return torch.triu(torch.ones((n,n)), diagonal=1)
 
-def train(base_llm, decoder, mem_layer, train_dataloader, num_epochs, PAD_IDX, dim_emb, max_len, device="cuda"):
+def pooling_fn(a):
+  torch.mean(a, dim=-2)
+
+def train(base_llm, decoder, train_dataloader, num_epochs, PAD_IDX, dim_emb, max_len, device="cuda"):
   base_llm = base_llm.to(device)
   decoder = decoder.to(device)
   optimizer = torch.optim.SGD(decoder.parameters(), lr=0.01)
@@ -41,8 +44,7 @@ def train(base_llm, decoder, mem_layer, train_dataloader, num_epochs, PAD_IDX, d
           tgt_mask = nn.Transformer.generate_square_subsequent_mask(tgt.size()[1]).bool().to(device)
 
           embedded_tgt = embed_fn(tgt)
-          memories = mem_layer(pooled, torch.concat(memories, dim=0), torch.concat(keys, dim=0))
-          probabilities = decoder(embedded_tgt, encoding, memories, memory_masks, tgt_mask)
+          probabilities = decoder(embedded_tgt, encoding, torch.concat(memories, dim=0), torch.concat(keys, dim=0), torch.concat(memory_masks, dim=0), tgt_mask)
 
           loss = loss_fn(torch.transpose(probabilities, 1, 2), truth) #need (batches, classes, seq). Before transpose, is (bathces, seq, classes)
           loss.backward()
@@ -79,14 +81,14 @@ data = load_dataset('silver/personal_dialog')
 train_data = process_data(data['train'], tokenizer, 100, max_len = max_len)
 decoder_layer = nn.TransformerDecoderLayer(d_model=hidden_size, nhead=8, batch_first=True)
 norm_layer = nn.LayerNorm(hidden_size)
-decoder = ManualDecoder(nn.TransformerDecoder(decoder_layer, num_layers = 4, norm = norm_layer), hidden_size, vocab_size)
+decoder = ManualDecoder(nn.TransformerDecoder(decoder_layer, num_layers = 4, norm = norm_layer), hidden_size, vocab_size, pooling_fn)
 
-decoder = train(pretrained_model, decoder, train_data, 10, config.pad_token_id, device)
+decoder = train(pretrained_model, decoder, train_data, 10, config.pad_token_id, hidden_size, max_len, device)
 torch.save(decoder.state_dict(), "decoder10")
-decoder = train(pretrained_model, decoder, train_data, 10, config.pad_token_id, device)
+decoder = train(pretrained_model, decoder, train_data, 10, config.pad_token_id, hidden_size, max_len, device)
 torch.save(decoder.state_dict(), "decoder20")
-decoder = train(pretrained_model, decoder, train_data, 10, config.pad_token_id, device)
+decoder = train(pretrained_model, decoder, train_data, 10, config.pad_token_id, hidden_size, max_len, device)
 torch.save(decoder.state_dict(), "decoder30")
-decoder = train(pretrained_model, decoder, train_data, 10, config.pad_token_id, device)
+decoder = train(pretrained_model, decoder, train_data, 10, config.pad_token_id, hidden_size, max_len, device)
 torch.save(decoder.state_dict(), "decoder40")
-chatbot = FineTuneTransformer(pretrained_model, decoder, hidden_size, 101, 102, device)
+chatbot = FineTuneTransformer(pretrained_model, decoder, hidden_size, 101, 102, hidden_size, max_len, device)
--- model2.py
+++ model2.py
@@ -24,32 +24,36 @@ class Memory(nn.Module):
         to_dot = torch.unsqueeze(to_dot, dim=-2).repeat([1, n_mems, 1] if self.batched else [n_mems, 1])
         #to_dot (n, mems, dim) or (mems, dim)
         weights = torch.softmax(torch.sum(to_dot * keys, dim=-1), dim=-1) #dot product along last dimension
-        scaled_memories = torch.einsum('nmsd,nm->nmsd', memories, weights) #scale each memory
+        scaled_memories = torch.einsum('nmsd,nm->nmsd' if self.batched else 'msd,m->msd', memories, weights) #scale each memory
         return torch.transpose(scaled_memories, 0, 1) if self.batched else scaled_memories #(mems, n, seq, dim)
 
 class ManualDecoder(nn.Module):
-    def __init__(self, layer, N):
+    def __init__(self, layer, N, batched, pooling_fn):
         super(ManualDecoder, self).__init__()
         self.layer = layer
         self.layers = clones(layer, N - 1)
         self.norm = nn.LayerNorm(layer.dim_emb)
+        self.memory_layer = Memory(layer.dim_emb, batched=batched)
+        self.pooler = pooling_fn
 
-    def forward(self, x, encoded, memory, memory_masks, tgt_mask, tgt_padding_mask):
-        output = self.layer(x, encoded, tgt_mask=tgt_mask, tgt_key_padding_mask=tgt_padding_mask)
-        for mem, mem_mask in zip(memory, memory_masks):
+    def forward(self, tgt, encoded, memories, mem_keys, memory_masks, tgt_mask, tgt_padding_mask):
+        memories = self.memory_layer(self.pooler(encoded), memories, mem_keys)
+        output = self.layer(tgt, encoded, tgt_mask=tgt_mask, tgt_key_padding_mask=tgt_padding_mask)
+        for mem, mem_mask in zip(memories, memory_masks):
             output = self.layer.forward(output, mem, mem_mask)
         for layer in self.layers:
             output = layer.forward(output, encoded, tgt_mask, tgt_padding_mask)
         return self.norm(output)
 
 class FineTuneTransformer(nn.Module):
-    def __init__(self, LLM, decoder, emb_size, tgt_vocab_size, batched=False, dim_feedforward = 512, dropout = 0.1):
+    def __init__(self, LLM, decoder, emb_size, tgt_vocab_size, BOS_token_id, EOS_token_id, dim_feedforward = 512, dropout = 0.1):
         super(FineTuneTransformer, self).__init__()
         self.encoder = LLM
         self.decoder = decoder
         self.embed = LLM.get_input_embeddings()
         self.out = nn.Linear(emb_size, tgt_vocab_size)
-        self.memory = Memory(emb_size, batched)
+        self.BOS = BOS_token_id
+        self.EOS = EOS_token_id
     
     def get_src_mask(self, src):
         # Essentially no masking
@@ -63,20 +67,32 @@ class FineTuneTransformer(nn.Module):
         mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
         return mask
     
-    def encode(self, src, src_padding_mask, token_type_ids):
-        output = self.encoder(input_ids=src, attention_mask=src_padding_mask, token_type_ids=token_type_ids)
-        return output.last_hidden_state, output.pooler_output
+    def encode(self, input_ids, attention_mask, token_type_ids):
+        output = self.encoder(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        return output.last_hidden_state
 
-    def decode(self, tgt, encoded, memory, memory_mask, src_padding_mask, tgt_padding_mask):
+    def decode(self, tgt, encoded, memories, mem_keys, memory_mask, src_padding_mask, tgt_padding_mask):
         output = self.decoder(
-            self.embed(tgt), encoded, memory, memory_mask, 
-            src_padding_mask, self.get_tgt_mask(tgt), tgt_padding_mask, self.training)
+            self.embed(tgt), encoded, memories, mem_keys, memory_mask, 
+            self.get_tgt_mask(tgt), tgt_padding_mask)
         return output
     
-    def forward(self, src, tgt, kv_store, keys, memory_masks, src_padding_mask, tgt_padding_mask):
+    def forward(self, input_ids, kv_store, keys, memory_masks, attention_mask):
         # Note that this implementation is for teacher forcing.
-        encoded, pooled = self.encode(src, src_padding_mask)
-        memories = self.memory(pooled, kv_store, keys)
-        decoded = self.decode(tgt, encoded, memories, memory_masks, src_padding_mask, tgt_padding_mask)
-        output = self.out(decoded)
-        return output
+        input_ids = input_ids.to(self.device)
+        attention_mask = attention_mask.to(self.device)
+        with torch.no_grad():
+            encoding = self.llm(input_ids=input_ids, attention_mask=attention_mask)
+            encoded_input = encoding.last_hidden_state
+            out_seq = [[self.BOS]]
+            while out_seq[0][-1] != self.EOS or len(out_seq[0] > 200):
+                tgt = self.embed(torch.tensor(out_seq, dtype=torch.long, device=self.device))
+                #print(tgt.size())
+                tgt_mask = nn.Transformer.generate_square_subsequent_mask(tgt.size()[1]).bool().to(self.device)
+                #print(mask.size())
+                response_logits = self.decoder(tgt, encoded_input, kv_store, keys, memory_masks, tgt_mask, attention_mask)
+                #print(response_logits.size())
+                token_id = torch.argmax(response_logits[:, -1, :], dim=-1)
+                #print("---------------------------------")
+                out_seq[0].append(token_id.item())
+            return out_seq
