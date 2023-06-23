7fa9ea95da066b794f52bbd4b56eec195235bc3c
SolarPowerWu <solarpowerwu@gmail.com>
2023-06-22 21:45:49 -0500

    dataset changes, max_len

--- data.py
+++ data.py
@@ -3,20 +3,19 @@ from transformers import AutoTokenizer
 import torch
 from tqdm import tqdm
 
-def process_data(dataset, tokenizer, n):
+def process_data(dataset, tokenizer, n, max_len):
     dialogue = None
-    profiles = None
-    uids = None
     out = []
-    j = 0
-    for entry in tqdm(dataset):
-        if j > n:
+    count = 0
+    for entry in tqdm(dataset[:n]):
+        if count > n:
             break
+        if len(entry['uid']) < 3:
+            continue
         dialogue = entry['dialog']
         #profiles = entry['profile']
         #uids = entry['uid']
-        tokenized = [tokenizer(sentence.replace(" ", ""), return_tensors="pt") for sentence in dialogue]
-        for i in range(len(tokenized) - 1):
-            out.append((tokenized[i], tokenized[i + 1]['input_ids']))
-        j += 1
+        tokenized = [tokenizer(sentence.replace(" ", ""), return_tensors="pt", padding = 'max_length', max_length = max_len) for sentence in dialogue]
+        out.append(tokenized)
     return out
+    
