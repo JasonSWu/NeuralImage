0b603ecb6595078c41438bd70615576372ad0695
SolarPowerWu <solarpowerwu@gmail.com>
2023-06-22 21:52:11 -0500

    debugging

--- main.py
+++ main.py
@@ -78,6 +78,7 @@ max_len = 271 #541 with spaces
 config.pad_token_id = 0
 
 data = load_dataset('silver/personal_dialog')
+print(data)
 train_data = process_data(data['train'], tokenizer, 100, max_len = max_len)
 decoder_layer = nn.TransformerDecoderLayer(d_model=hidden_size, nhead=8, batch_first=True)
 norm_layer = nn.LayerNorm(hidden_size)
