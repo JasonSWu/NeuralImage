29ea60b0de5fd5fe3ff98dea94070891b42b9b76
SolarPowerWu <solarpowerwu@gmail.com>
2023-06-27 12:03:02 -0500

    10000 examples

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ max_len = 271 #541 with spaces
 config.pad_token_id = 0
 
 data = load_dataset('silver/personal_dialog')
-train_data = process_data(data['train'], tokenizer, 10, max_len = max_len)
+train_data = process_data(data['train'], tokenizer, 10000, max_len = max_len)
 decoder_layer = nn.TransformerDecoderLayer(d_model=hidden_size, nhead=8, batch_first=True)
 norm_layer = nn.LayerNorm(hidden_size)
 decoder = ManualDecoder(decoder_layer, 3, True, hidden_size, vocab_size, pooling_fn)
