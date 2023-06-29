ef1fa182de1e927708d1382e8e7232b11c562caf
SolarPowerWu <solarpowerwu@gmail.com>
2023-06-29 15:39:02 -0500

    add back memory

--- model2.py
+++ model2.py
@@ -46,8 +46,8 @@ class ManualDecoder(nn.Module):
         #tgt_padding_mask(bsz, max_seq-1)
         #memories = self.memory_layer(self.pooler(encoded), memories, mem_keys)
         output = self.layer(tgt, encoded, tgt_mask, tgt_key_padding_mask=tgt_padding_mask, memory_key_padding_mask = src_padding_mask)
-        #for mem, mem_pad_mask in zip(memories, memory_padding_masks):
-        #    output = self.layer.forward(output, mem, tgt_mask, tgt_key_padding_mask = tgt_padding_mask, memory_key_padding_mask = mem_pad_mask)
+        for mem, mem_pad_mask in zip(memories, memory_padding_masks):
+            output = self.layer.forward(output, mem, tgt_mask, tgt_key_padding_mask = tgt_padding_mask, memory_key_padding_mask = mem_pad_mask)
         for layer in self.layers:
             output = layer.forward(output, encoded, tgt_mask, tgt_key_padding_mask = tgt_padding_mask, memory_key_padding_mask = src_padding_mask)
         return self.lin(self.norm(output))
