403aa0dcf19d68ad60bc8620412069e3811aac6a
SolarPowerWu <solarpowerwu@gmail.com>
2023-07-04 10:17:56 -0500

    test learning rate change when load optim

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 import torch
 import torch.nn as nn
-from transformers import AutoConfig, AutoTokenizer, XLMRobertaModel, AutoModelForCausalLM
-from model import MyDecoder
+from tqdm import tqdm
+from datasets import load_dataset
+from transformers import AutoConfig, AutoTokenizer, AutoModelForCausalLM
+from model import ChatBot, MyDecoder
+from model2 import FineTuneTransformer, ManualDecoder
+from data import process_data
+import os, sys
+
+def pooling_fn(a):
+  return torch.mean(a, dim=-2)
 
 device = torch.device("cuda")
 
@@ -9,16 +17,13 @@ config = AutoConfig.from_pretrained("Alethea/GPT2-chitchat")
 tokenizer = AutoTokenizer.from_pretrained("Alethea/GPT2-chitchat")
 pretrained_model = AutoModelForCausalLM.from_pretrained("Alethea/GPT2-chitchat")
 pretrained_model.eval()
-for param in pretrained_model.parameters():
-    param.requires_grad = False
+
 hidden_size = config.hidden_size
 vocab_size = config.vocab_size
 
-decoder_layer = nn.TransformerDecoderLayer(d_model=hidden_size, nhead=8, batch_first=True, dropout=0.02)
-norm_layer = nn.LayerNorm(hidden_size)
-decoder = MyDecoder(nn.TransformerDecoder(decoder_layer, num_layers = 4, norm = norm_layer), hidden_size, vocab_size)
-
-def count_parameters(model):
-    return sum([p.numel() / 1000000 for p in model.parameters() if p.requires_grad])
-print(count_parameters(decoder))
-print(count_parameters(pretrained_model))
+decoder_layer = nn.TransformerDecoderLayer(d_model=hidden_size, nhead=8, batch_first=True)
+decoder = ManualDecoder(decoder_layer, 3, True, hidden_size, vocab_size, pooling_fn)
+optimizer = torch.optim.AdamW(decoder.parameters(), lr=0.1)
+optimizer.load_state_dict(torch.load("./optimizer80"))
+print(optimizer.param_groups[0]['lr'])
+loss_fn = torch.nn.CrossEntropyLoss(ignore_index=config.pad_token_id) #Ignore padding, dont let it contribute to training
