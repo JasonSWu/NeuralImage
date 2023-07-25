d02be7326f00019d0bce94b236535897e2433ba4
SolarPowerWu <solarpowerwu@gmail.com>
2023-07-24 23:58:51 -0500

    tweak prompt

--- search.py
+++ search.py
@@ -11,7 +11,7 @@ def get_info_identifier(model='text-davinci-001'):
     openai.api_key = os.environ["OPENAI_API_KEY"]
 
     template = """
-    If the chat requires specialized expert knowledge, 
+    If the chat requires specialized knowledge, 
     output a question as if you are asking for this information from a search engine.
     Otherwise, especially if the chat requires only personal information to respond to,
     output "Nothing." 
