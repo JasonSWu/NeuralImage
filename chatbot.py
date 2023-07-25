95c37d4b16de3b2136cf006b786df8c1914935f7
SolarPowerWu <solarpowerwu@gmail.com>
2023-07-24 23:49:51 -0500

    change info identifier model

--- chatbot.py
+++ chatbot.py
@@ -72,7 +72,7 @@ qa = ConversationalRetrievalChain.from_llm(
     combine_docs_chain_kwargs={"prompt": PROMPT},
 )
 
-info_identifier = get_info_identifier('text-curie-001')
+info_identifier = get_info_identifier('text-davinci-001')
 summarizer = get_summarizer('text-curie-001')
 
 max_len = 50
