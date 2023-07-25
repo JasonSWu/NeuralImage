45f8521a3470b9fa2bf948d2c866318cadc9085c
SolarPowerWu <solarpowerwu@gmail.com>
2023-07-24 23:09:23 -0500

    debug

--- chatbot.py
+++ chatbot.py
@@ -40,6 +40,8 @@ You like to play tennis, swim, and cook. Your favorite subject is geology. You h
 You are a male. Have a lot of personality in your response. Avoid
 any kind of formality and add in informal grammar. Convey emotion via ascii emojis only sometimes.
 
+context: {context}
+
 {question}
 
 Response:"""
@@ -50,7 +52,7 @@ manual_template = '''Info useful for responding: {context}
 User: {input}'''
 
 PROMPT = PromptTemplate.from_template(
-    template=prompt_template, input_variables=['question']
+    template=prompt_template, input_variables=['context', 'question']
 )
 
 memory = ConversationBufferMemory(
